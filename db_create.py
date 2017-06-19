f2fa27dd6258cb2a0e5a750b16a64c137f861f57
gd8 <gdanielsson@me.com>
2017-06-18 21:04:01 -0400

    Delete db files from root directory

--- db_create.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-connection = sqlite3.connect('data.db')
-cursor = connection.cursor()
-
-create_table = "CREATE TABLE IF NOT EXISTS todos (id INTEGER PRIMARY KEY, done text, description text)"
-
-cursor.execute(create_table)
-connection.close()
