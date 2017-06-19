b94afe8d682f92683e8d25031ec4386d984c8f29
gd8 <gdanielsson@me.com>
2017-06-19 00:29:42 -0400

    Modify footer component to disallow mark all when no items left

--- static/js/components/todo_footer.js
+++ static/js/components/todo_footer.js
@@ -6,18 +6,27 @@ class TodoFooter extends Component {
         this.state = {items_left: 0};
     }
 
-    countItemsLeft() {
-        let items_left = this.props.todos.reduce((items_left, todo) =>{
+    componentDidMount() {
+        this.countItemsLeft(this.props.todos);
+    }
+
+    componentWillReceiveProps(nextProps) {
+        this.countItemsLeft(nextProps.todos);
+    }
+
+    countItemsLeft(todos) {
+        let items_left = todos.reduce((items_left, todo) =>{
             return items_left + (todo.done ? 0 : 1);
         }, 0);
-        return items_left;
+        this.setState({items_left: items_left});
     }
 
     render() {
         return (
             <div className="footer container">
-                <span className="text-muted">{this.countItemsLeft()} items left</span>
-                <a  className="pointer-right" onClick={this.props.markAllComplete}>
+                <span className="text-muted">{this.state.items_left} items left</span>
+                <a  className={this.state.items_left == 0 ? "disabled-link" : "pointer-right"} 
+                    onClick={this.props.markAllComplete}>
                     Mark all as complete
                 </a>
             </div>
