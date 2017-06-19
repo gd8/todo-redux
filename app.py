9380c0e37b49bd92863283547d73211648ce209c
gd8 <gdanielsson@me.com>
2017-06-18 21:03:48 -0400

    Modify app.py to serve index.html

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_restful import Api
 
 from resources.todo_list import TodoList
@@ -9,6 +9,10 @@ api = Api(app)
 
 api.add_resource(TodoList, '/todos')
 api.add_resource(Todo, '/todo')
+
+@app.route('/')
+def TodoApp():
+  return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
