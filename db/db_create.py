fc2a0ff1581d19bdb582f38a7c9061b80e57c2b7
gd8 <gdanielsson@me.com>
2017-06-27 12:50:07 -0400

    Modify done column to be boolean-like bit type

--- db/db_create.py
+++ db/db_create.py
@@ -1,6 +1,6 @@
 import sqlite3
 from db_connection import DBConnection
 
-create_table = "CREATE TABLE IF NOT EXISTS todos (id INTEGER PRIMARY KEY, done text, description text)"
+create_table = "CREATE TABLE IF NOT EXISTS todos (id INTEGER PRIMARY KEY, done BOOLEAN NOT NULL CHECK (done IN (0,1)), description text)"
 db = DBConnection()
 db.modify(create_table)
