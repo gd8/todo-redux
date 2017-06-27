fc79ee68f7d210384996aad44daa2f641d2fec13
gd8 <gdanielsson@me.com>
2017-06-27 12:48:44 -0400

    Update npm/webpack/babel config for redux libraries

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
         test: /\.js?$/,
         loader: 'babel-loader',
         query: {
-          presets: ['es2015', 'react']
+          presets: ['es2015', 'react', 'stage-1']
         },
         exclude: /node_modules/
       },
